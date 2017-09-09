3b095c650db8c3395ae51f6aaee8e4a64ca0745e
matt2112 <matt_lewis@live.co.uk>
2017-09-09 08:04:09 +0100

    initial commit

---
+++ src/js/components/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => (
+  <div className="wrapper">
+    <h1 className="title">Capital Cities Quiz</h1>
+    <div className="main">
+      This is the main section.
+    </div>
+    <footer className="footer">
+      Coded by <a href="https://github.com/matt2112" target="_blank" rel="noreferrer noopener">Matt Lewis</a>
+    </footer>
+  </div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+  'react', 'react-dom', 'react-redux', 'redux',
+];
+
+module.exports = {
+  entry: {
+    bundle: './src/js/index.js',
+    vendor: VENDOR_LIBS,
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js',
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        test: /\.scss$/,
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/,
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|ico)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 40000 },
+          },
+          'image-webpack-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest'],
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+  ],
+};
